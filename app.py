04829b1fb6b0b943b835cdaf33357f63d397319f
rachaelwanja <mararachel85@gmail.com>
2026-03-08 01:40:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def whatsapp():
 
     try:
         response = client.models.generate_content(
-            model="gemini-2.0-flash",
+            model="gemini-1.5-flash-8b"
             contents=incoming_msg
         )
 
