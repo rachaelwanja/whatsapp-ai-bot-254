d8587edda18b615ca38973413795fff88756abb2
rachaelwanja <mararachel85@gmail.com>
2026-06-23 15:16:22 +0300

    Update models.py

--- models.py
+++ models.py
@@ -98,3 +98,70 @@ class Appointment(db.Model):
         db.DateTime,
         default=datetime.utcnow
     )
+class Service(db.Model):
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    business_id = db.Column(
+        db.Integer
+    )
+
+    name = db.Column(
+        db.String(200)
+    )
+
+    price = db.Column(
+        db.Integer,
+        default=0
+    )
+
+    duration = db.Column(
+        db.String(100),
+        default="30 mins"
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
+
+class Payment(db.Model):
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    business_id = db.Column(
+        db.Integer
+    )
+
+    customer_name = db.Column(
+        db.String(200)
+    )
+
+    customer_phone = db.Column(
+        db.String(100)
+    )
+
+    amount = db.Column(
+        db.Integer
+    )
+
+    mpesa_receipt = db.Column(
+        db.String(100),
+        default=""
+    )
+
+    status = db.Column(
+        db.String(50),
+        default="Pending"
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
