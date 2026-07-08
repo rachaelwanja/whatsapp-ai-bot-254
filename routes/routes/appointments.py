e619b67337d5996ff21ea78ac59ce6d113748494
rachaelwanja <mararachel85@gmail.com>
2026-07-08 23:19:30 +0300

    Add imports and initialize appointments blueprint

--- routes/routes/appointments.py
+++ routes/routes/appointments.py
@@ -1,1 +1,19 @@
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    redirect,
+    session,
+    flash
+)
 
+from models import (
+    db,
+    Business,
+    Appointment
+)
+
+appointments = Blueprint(
+    "appointments",
+    __name__
+)
