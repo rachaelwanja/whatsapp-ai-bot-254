36db8d01658100723d278398f13f53b27fdd212b
rachaelwanja <mararachel85@gmail.com>
2026-07-08 14:27:02 +0300

    Update services.py

--- routes/services.py
+++ routes/services.py
@@ -1,1 +1,25 @@
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    redirect,
+    session,
+    flash
+)
+
+from werkzeug.utils import secure_filename
+
+from models import (
+    db,
+    Business,
+    Service
+)
+
+import os
+import uuid
+
+services = Blueprint(
+    "services",
+    __name__
+)
 
