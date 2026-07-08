efcac36a2422bceb989b10fec742bfa1159888a3
rachaelwanja <mararachel85@gmail.com>
2026-07-08 23:59:41 +0300

    Add functionality to create new appointments

--- routes/appointments.py
+++ routes/appointments.py
@@ -45,3 +45,74 @@ def appointments_page():
         business=business,
         appointments=appointments
     )
+
+# =========================================
+# ADD APPOINTMENT
+# =========================================
+
+@appointments.route("/add-appointment", methods=["POST"])
+def add_appointment():
+
+    if "business_id" not in session:
+        return redirect("/login")
+
+    business_id = session["business_id"]
+
+    appointment_time = request.form.get(
+        "appointment_time"
+    )
+
+    existing = Appointment.query.filter_by(
+
+        business_id=business_id,
+
+        appointment_time=appointment_time
+
+    ).first()
+
+    if existing:
+
+        flash(
+            "Time slot already booked"
+        )
+
+        return redirect(
+            "/dashboard"
+        )
+
+    appointment = Appointment(
+
+        business_id=business_id,
+
+        customer_name=request.form.get(
+            "customer_name"
+        ),
+
+        customer_phone=request.form.get(
+            "customer_phone"
+        ),
+
+        service=request.form.get(
+            "service"
+        ),
+
+        amount=int(
+            request.form.get("amount")
+        ),
+
+        appointment_time=appointment_time
+
+    )
+
+    db.session.add(
+        appointment
+    )
+
+    db.session.commit()
+
+    flash("Appointment added successfully!")
+
+    return redirect(
+        "/dashboard"
+    )
+
