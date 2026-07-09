cf9d3b33573920cd47a1394a07bcd400f4ed4c6c
rachaelwanja <mararachel85@gmail.com>
2026-07-09 15:11:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -166,25 +166,6 @@ def migrate_business():
         return str(e)
     
 # =========================================
-# WHATSAPP AI
-# =========================================
-
-@app.route("/whatsapp-ai")
-def whatsapp_ai():
-
-    if "business_id" not in session:
-        return redirect("/login")
-
-    business = Business.query.get(
-        session["business_id"]
-    )
-
-    return render_template(
-        "whatsapp_ai.html",
-        business=business
-    )
-    
-# =========================================
 # ANALYTICS
 # =========================================
 
